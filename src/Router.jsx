9ad058d73c57b055c659ce74a358ecfbfc7099d3
Ignacio Prieto <ignacio.prieto.s@gmail.com>
2026-02-23 13:21:13 -0300

    nueva ruta

--- src/Router.jsx
+++ src/Router.jsx
@@ -29,8 +29,8 @@
                                 <Route path="/iniciar-sesion" element={<AuthRoute component={Login} />} />
                                 <Route path="/perfil" element={<PrivateRoute component={Profile} />} />
                                 <Route path="/carrito" element={<PrivateRoute component={Checkout} />} />
-                                <Route path="/carrito/success" element={<PrivateRoute component={Success} />} />
-                                <Route path="/carrito/cancel" element={<PrivateRoute component={Cancel} />} />
+                                <Route path="/pago-exitoso/success" element={<PrivateRoute component={Success} />} />
+                                <Route path="/pago-cancelado/cancel" element={<PrivateRoute component={Cancel} />} />
                             </Route>
                         </Routes>
                     </BrowserRouter>
